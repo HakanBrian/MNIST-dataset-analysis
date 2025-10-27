aeec4bf26948c9a5349c854c316fb06449ff6df0
HakanBrian <140771010+HakanBrian@users.noreply.github.com>
2025-10-27 17:00:11 -0500

    feat: add slurm script

---
+++ MNIST-dataset-analysis.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --time=1-00:00:00           # Time limit for the job (REQUIRED).
+#SBATCH --job-name=MNIST-analysis   # Job name
+#SBATCH --nodes=1                   # Number of nodes to allocate. Same as SBATCH -N (Don't use this option for mpi jobs)
+#SBATCH --exclusive                 # Allocate all cores in node.
+#SBATCH --partition=normal          # Partition/queue to run the job in. (REQUIRED)
+#SBATCH -e slurm-%j.err             # Error file for this job.
+#SBATCH -o slurm-%j.out             # Output file for this job.
+#SBATCH -A <your project account>   # Project allocation account name (REQUIRED)
+#SBATCH --mail-type ALL             # Send email when job starts/ends
+#SBATCH --mail-user <your email>    # Email address to send email to
+
+module purge                        # Unload other software modules
+module load module load ccs/conda/python
+
+python MNIST-dataset-analysis.py
